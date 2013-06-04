c4e838792e204f52e08aa68332a6b18bec87aca3
Yago Pérez Vázquez <yagoperez@outlook.com>
2013-06-04 18:37:57 +0200

    renamed config to build and rebuild all

--- build.bat
+++ build.bat
@@ -1,9 +1,11 @@
 copy src\Stashy.*.* docs\public\App\stashy  /Y
 copy css\Stashy.*.* docs\public\Content  /Y
 
-node config.js
+node build.js
 
-docs/public/App/durandal/amd/optimizer.exe
+cd docs/public/App/durandal/amd
+
+optimizer.exe
 
 pause
 
