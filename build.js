80c8552d178e401371562ba2668e07c572f84f43
Yago Pérez Vázquez <yagoperez@outlook.com>
2013-10-10 07:11:04 -0400

    Deleted build.js

--- build.js
+++
@@ -1,7 +0,0 @@
-var optimize = require('./modules/optimize');  
-
-// Minify & bundle project assets under css and src directories  
-optimize.optimizeProjectAssets();
-
- // Minify & bundle documents assets under Scripts and Content web site directories
-optimize.optimizeDocsAssets();
